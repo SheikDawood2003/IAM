b39bbd987ea05d1061b6ee819f5936ba5aedb869
Sheik Dawood <sheikdawoodaym1@gmail.com>
2024-08-27 08:39:24 +0530

    commit

--- Admin/Home/Aids/addstudent.php
+++ Admin/Home/Aids/addstudent.php
@@ -21,6 +21,7 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $username = $_POST['username'];
     $email = $_POST['email'];
     $password = $_POST['password'];
+    $password= md5($_POST['password']);
 
     do{
         if(empty($username) || empty($email) || empty($password)){
